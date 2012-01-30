4640ef4c700756ab9ba1fd4f4b4820cb44372666
Satoshi OOMORI <oogon@mac.com>
2012-01-30 20:41:20 +0900

    Initial Commit

---
+++ NSString/OOOAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OOOAppDelegate.h
+//  NSString
+//
+//  Created by 大森 智史 on 12/01/30.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface OOOAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (strong) IBOutlet NSWindow *window;
+
+@end
---
+++ NSString/OOOAppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  OOOAppDelegate.m
+//  NSString
+//
+//  Created by 大森 智史 on 12/01/30.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import "OOOAppDelegate.h"
+
+@implementation OOOAppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
